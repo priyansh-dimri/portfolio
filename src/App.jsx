8801fae6e486000d47d0e9dffcb4815933b5b055
Priyansh Dimri <priyanshd0908@gmail.com>
2025-03-30 15:49:56 +0530

    fix navbarpanel expanding
    - Change color of moon and sun to emerald-400

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import NavPanel from "./layouts/NavPanel";
 function App() {
   return (
     <div
-      className="min-h-screen flex flex-col sm:pb-8 lg:grid lg:grid-cols-[1fr_4fr]
+      className="min-h-screen flex flex-col sm:pb-8 lg:grid lg:grid-cols-[1fr_4fr] lg:items-start
       px-6 pt-8 pb-14 gap-4 
       bg-slate-100 text-slate-900 dark:bg-zinc-900 dark:text-zinc-100"
     >
